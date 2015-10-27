3d8a74bef87342f2b18e8cc98ea46b307a64f9c0
robertoyubero <robertoyubero@gmail.com>
2015-10-27 20:26:58 +0100

    apartado6

--- server.py
+++ server.py
@@ -8,22 +8,43 @@ import socketserver
 import sys
 
 
-class EchoHandler(socketserver.DatagramRequestHandler):
+class SIPRegisterHandler(socketserver.DatagramRequestHandler):
     """
     Echo server class
     """
+    dicc_clientes = {}
+
+    def get_nombre(self, mensaje):
+        nombre = mensaje.split(" ")[1]
+        nombre = nombre.split(":")[1]
+        return nombre
+
 
     def handle(self):
         # Escribe dirección y puerto del cliente (de tupla client_address)
         dir_client = self.client_address[0]
         port_client = self.client_address[1]
-        print("Direccion: " + dir_client +"  Puerto: " + str(port_client))
 
-        self.wfile.write(b"Hemos recibido tu peticion")
+        line = self.rfile.read()
+
+        #si es REGISTER debemos guardar el cliente
+        mensaje_recibido = line.decode('utf-8')
+        tipo_mensaje = mensaje_recibido.split(' ')[0]
+        if tipo_mensaje == "REGISTER":
+            print("El cliente nos manda " + tipo_mensaje)
+            #guardo al cliente
+            nombre = self.get_nombre(mensaje_recibido)
+            self.dicc_clientes[nombre] = dir_client
+            #traza
+            print(self.dicc_clientes)
+            self.wfile.write(b"SIP/2.0 200 OK\r\n\r\n")
+
+
         while 1:
             # Leyendo línea a línea lo que nos envía el cliente
             line = self.rfile.read()
-            print("El cliente nos manda " + line.decode('utf-8'))
+            mensaje_recibido = line.decode('utf-8')
+            print("El cliente nos manda " + mensaje_recibido)
 
             # Si no hay más líneas salimos del bucle infinito
             if not line:
@@ -31,6 +52,6 @@ class EchoHandler(socketserver.DatagramRequestHandler):
 
 if __name__ == "__main__":
     # Creamos servidor de eco y escuchamos
-    serv = socketserver.UDPServer(('', int(sys.argv[1])), EchoHandler)
+    serv = socketserver.UDPServer(('', int(sys.argv[1])), SIPRegisterHandler)
     print("Lanzando servidor UDP de eco...")
     serv.serve_forever()
