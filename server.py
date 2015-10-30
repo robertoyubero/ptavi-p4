735ebc964db55127f3fa90745f56f61c4d79f33f
robertoyubero <robertoyubero@gmail.com>
2015-10-30 17:13:54 +0100

    ejercicio10

--- server.py
+++ server.py
@@ -17,15 +17,22 @@ class SIPRegisterHandler(socketserver.DatagramRequestHandler):
     dicc_clientes = {}
 
     def json2registered(self):
-        #1)comprobar si existe el fichero registered.json
-        #1.1)si hay problema al leer suponemos que el fichero json no existe
-        #2)si el fichero existe copio su contenido en un diccionario de usuarios
-
-        pass
-
+        """
+        Extraigo la informacion de mis clientes de un fichero json
+        """
+        try:
+            fichero = open('registered.json')
+            print("True >> el fichero existe")
+            content = json.load(fichero)
+            print(len(content))
+            print("--------------------------")
+        except FileNotFoundError:
+            print("False >> el fichero no existe")
 
     def registered2json(self,nombre, dir_maquina, t_exp):
-        #devolver en fichero json nombre+direccion+t_expiracion
+        """
+        Guardo mis clientes en un fichero json
+        """
         dicc = {}
         dicc['adress'] = dir_maquina
         dicc['expires'] = t_exp
@@ -46,6 +53,9 @@ class SIPRegisterHandler(socketserver.DatagramRequestHandler):
 
 
     def get_time_expiration(self, t_expiracion):
+        """
+        Calculo cuando expirará mi sesión
+        """
         #le sumo una hora a la hora actual
         hora_expiracion = time.time() + 3600
         #le sumo el t_expiracion a la hora
@@ -54,10 +64,16 @@ class SIPRegisterHandler(socketserver.DatagramRequestHandler):
         return hora_expiracion
 
     def get_nombre(self, mensaje):
+        """
+        Extraigo el nombre del usuario del mensaje
+        """
         nombre = mensaje.split(" ")[1]
         nombre = nombre.split(":")[1]
         return nombre
     def get_expires(self, mensaje):
+        """
+        Extraigo el tiempo de expiracion del mensaje
+        """
         time = mensaje.split("Expires: ")[1]
         time = time.split("\r")[0]
         return(int(time))
@@ -78,9 +94,12 @@ class SIPRegisterHandler(socketserver.DatagramRequestHandler):
             nombre = self.get_nombre(mensaje_recibido)
             t_expires = self.get_expires(mensaje_recibido)
             hora_expiracion = self.get_time_expiration(t_expires)
+
+            #comprobamos si existe fichero
+            self.json2registered()
+
             #imprimimos fichero con register2json
             self.registered2json(nombre, local_host, hora_expiracion)
-
             #compruebo si debo borrar o no
             if t_expires == 0:
                 try:
